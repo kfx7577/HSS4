77fadee51f97fa8d83527b447ac82fca692402c0
Kenji Quik <kenjiquik@yahoo.com>
2016-02-11 18:06:30 +1300

    SSL Chat

---
+++ src/com/hss/assignment4/ChatMessage.java
@@ -0,0 +1,43 @@
+package com.hss.assignment4;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Kenji
+ */
+
+/*
+ * This class defines the different type of messages that will be exchanged between the
+ * Clients and the Server. 
+ * When talking from a Java Client to a Java Server a lot easier to pass Java objects, no 
+ * need to count bytes or to wait for a line feed at the end of the frame
+ */
+public class ChatMessage implements Serializable {
+
+    /* The different types of message sent by the Client
+    /* WHOISIN to receive the list of the users connected
+    /* MESSAGE an ordinary message
+    /* LOGOUT to disconnect from the Server
+    */
+
+    static final int WHOISIN = 0, MESSAGE = 1, LOGOUT = 2;
+    private int type;
+    private String message;
+
+    // Constructor
+    ChatMessage(int type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
+    // Get type
+    int getType() {
+        return type;
+    }
+
+    // Get message
+    String getMessage() {
+        return message;
+    }
+}
